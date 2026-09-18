e98c7b928587cf76e5c3bea557938967b25fbdf0
Judy Fajardo <judynataliaf@gmail.com>
2026-09-18 10:40:42 -0500

    Mover credenciales de Terraform a variables y backend externo por seguridad

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket                      = "terraform-state-lab"
-    key                         = "fastapi-app/terraform.tfstate"
-    region                      = "us-east-1"
-    access_key                  = "test"
-    secret_key                  = "test"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    use_path_style               = true
-
-    endpoints = {
-      s3 = "http://localhost:4566"
-    }
-  }
+  backend "s3" {}
 }
+
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
+  access_key                  = var.aws_access_key
+  secret_key                  = var.aws_secret_key
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
